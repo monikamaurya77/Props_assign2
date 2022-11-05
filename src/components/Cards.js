d91516ffcf63ce017458d450253e940d5e370e4c
Monika Maurya <96534851+monikamaurya77@users.noreply.github.com>
2022-11-05 22:19:09 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,13 +3,13 @@ const Cards = (props) => {
     return(
         <>
         <div className="cards">
-<img className="logo" src={props.img}></img>
+<img className="logo" src={props.img} alt="logo"></img>
 <h2 className="heading">{props.name}</h2>
 <h4>{props.year}</h4>
 <button className="button">{props.time}</button>
 <h5>{props.type}</h5>
 <p>{props.detail}</p>
-<img className="bgimg" src={props.image}></img>
+<img className="bgimg" src={props.image} alt="bglogo"></img>
 
         </div>
         </>
